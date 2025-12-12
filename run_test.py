8f0c6864173e5ad6090f24d515657a76c7ac0b86
ASL <15945232+ASL114514@user.noreply.gitee.com>
2025-12-12 17:50:00 +0800

    update

--- run_test.py
+++ run_test.py
@@ -3,4 +3,4 @@ import os
 if __name__ == "__main__":
     # 运行 pytest，搜索当前目录及其子目录中的所有测试用例
     pytest.main()
-    os.system("allure generate ./temp -c -o ./report")
+    # os.system("allure generate ./temp -c -o ./report")
