c4cef58b8c9a430c1fa1794af92b10842bbbab83
ASL <15945232+ASL114514@user.noreply.gitee.com>
2025-12-12 15:28:09 +0800

    first commit

---
+++ common/requests_util.py
@@ -0,0 +1,21 @@
+import requests
+
+class RequestsUtil: #请求基类
+    session = requests.session()
+
+    def send_request(self, **kwargs):
+        total_params = {
+            "application": "web",
+            "application_client_type": "pc",
+        }
+        for key,value in kwargs.items():
+            if key == "params":
+                params = kwargs.get("params", {})
+                params.update(total_params) #字典中的update方法，如果键值不存在则会新增，如果键值已存在，则会覆盖原值
+                kwargs["params"] = params
+            elif key == "file":
+                for file_key, file_value in value.items():
+                        value[file_key] = open(file_value, "rb")    #这里是将原来的文件路径字符串用文件对象替换（在字典里面是字符串）
+        
+        response = RequestsUtil.session.request(**kwargs)
+        return response
---
+++ common/yaml_util.py
@@ -0,0 +1,53 @@
+import yaml
+import os
+# from data import ex
+
+# def write_yaml(data):
+#     with open("ex.yaml","w") as f:
+#         yaml.dump(data,f,allow_unicode=True)
+
+# def read_yaml(key):
+#     with open("ex.yaml", "r") as f:
+#         result = yaml.safe_load(f)
+#         return result[key]
+    
+# # 清空yaml文件内容
+# def clean_yaml():
+#     with open("ex.yaml", mode="w", encoding="utf-8") as f:
+#         pass
+
+# #测试用例中读取yaml，以实现数据驱动
+# def read_yaml_testcase(yaml_path):
+#     with open(yaml_path, "r", encoding="utf-8") as f:
+#         result = yaml.safe_load(f)
+#         return result
+    
+
+#上述写法是只能针对一个yaml文件进行读取和写入，下面的是针对多个yaml文件进行读取和写入
+
+class YamlUtil:
+
+    @staticmethod
+    def read_yaml(path,key):
+        with open(path, "r", encoding="utf-8") as f:
+            result = yaml.safe_load(f)
+            return result[key]
+
+    @staticmethod
+    def write_yaml(path, data): #此处的data是字典数据，不单单是值，是键值对
+        with open(path, "w", encoding="utf-8") as f:
+            yaml.dump(data, f, allow_unicode=True)
+
+    @staticmethod
+    def clean_yaml(path):
+        open(path, "w", encoding="utf-8").close()
+
+    @staticmethod
+    def extract(path):
+        """适用于获取 token 等变量"""
+        with open(path, "r", encoding="utf-8") as f:
+            data = yaml.safe_load(f)
+            return data
+
+
+    
